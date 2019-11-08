69d76ccf6f1e13935f95ad0f2687f220abcb79d5
YuriyVladimirovi4 <bas.bas.2019@list.ru>
2019-11-08 17:46:00 +0300

    Добавлены Кисти...

---
+++ BakingShop/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakingShop.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private int _Clicks;
+        public int Clicks
+        {
+            get { return _Clicks; }
+            set 
+            {
+                _Clicks = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+}
